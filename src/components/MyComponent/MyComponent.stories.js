eb3d68052420a01d1a5701b177ef2818c1bcd256
student <manjaka.ran@onja.org>
2021-04-23 07:36:48 +0300

    created editable text input and uneditable text input

--- src/components/MyComponent/MyComponent.stories.js
+++ src/components/MyComponent/MyComponent.stories.js
@@ -4,10 +4,10 @@ import { View } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 
-import MyCoponent from './MyComponent';
+import MyComponent from './MyComponent';
 
 
 storiesOf('MyComponent', module)
   .addDecorator(story => <View>{story()}</View>)
-  .add('number', () => <MyCoponent label= {'1, 2, 3, 4, 5, 6, 7, 8, 9, 10'}/>)
+  .add('number', () => <MyComponent label= {'1, 2, 3, 4, 5, 6, 7, 8, 9, 10'}/>)
   
--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -6,6 +6,7 @@
 function loadStories() {
   require('../src/components/Example/Example.stories');
   require('../src/components/MyComponent/MyComponent.stories');
+  require('../src/components/PhraseTextArea/PhraseTextArea.stories');
   require('./stories/Button/Button.stories');
   require('./stories/Welcome/Welcome.stories');
 }
@@ -13,6 +14,7 @@ function loadStories() {
 const stories = [
   '../src/components/Example/Example.stories',
   '../src/components/MyComponent/MyComponent.stories',
+  '../src/components/PhraseTextArea/PhraseTextArea.stories',
   './stories/Button/Button.stories',
   './stories/Welcome/Welcome.stories',
 ];
