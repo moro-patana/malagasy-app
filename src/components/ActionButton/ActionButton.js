ff55870b9f320f3cd88dab26bcfa4fe53da0a516
student <manjaka.ran@onja.org>
2021-04-24 10:41:56 +0300

    created actions buttons and added styles to each of them

--- src/components/ActionButton/ActionButton.js
+++ src/components/ActionButton/ActionButton.js
@@ -1,17 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { TouchableHighlight } from 'react-native';
+import * as React from "react"
+import { TouchableOpacity, Text, StyleSheet } from "react-native"
 
-export default function ActionButton({ onPress,style , children }) {
-  return <TouchableHighlight onPress={onPress} style={style}>{children}</TouchableHighlight>;
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#06B6D4",
+    paddingVertical: 11,
+    paddingHorizontal: 27,
+    borderRadius: 30,
+    margin: 20,
+  },
+  containerDisabled: {
+    backgroundColor: 'transparent',
+    borderColor: "#06B6D4",
+    borderWidth: 1,
+    borderColor: "#06B6D4",
+  },
+
+  text: {
+    fontFamily: "Inter",
+    fontStyle: "normal",
+    fontWeight: "600",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#FFFFFF",
+    textAlign: "center",
+  },
+  blueText: {
+    color: "#06B6D4",
+  },
+});
+
+export default function ActionButton({
+  children, 
+  onPress = () => {},
+  disabled = false
+}) {
+  const containerStyles = [styles.container];
+  const textStyles = [styles.text];
+
+  if (disabled) {
+    containerStyles.push(styles.containerDisabled);
+    textStyles.push(styles.blueText);
+  }
+  return (
+    <TouchableOpacity onPress={onPress} disabled={disabled ? true : false} style={containerStyles}>
+      <Text style={textStyles}>{children}</Text>
+    </TouchableOpacity>
+  )
 }
-
-ActionButton.defaultProps = {
-  children: null,
-  onPress: () => {},
-};
-
-ActionButton.propTypes = {
-  children: PropTypes.node,
-  onPress: PropTypes.func,
-};
