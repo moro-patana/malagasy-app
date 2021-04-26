5f7c263ac806cff389d885aff850037e3da1b077
student <manjaka.ran@onja.org>
2021-04-26 13:45:31 +0300

    using Icon element for the icon buttons

--- src/components/ActionButtons/ActionButtons.js
+++ src/components/ActionButtons/ActionButtons.js
@@ -1,18 +1,31 @@
 import * as React from "react"
-import { TouchableOpacity, StyleSheet } from "react-native"
+import { TouchableOpacity, StyleSheet, Text } from "react-native"
+import { Icon } from "react-native-elements"
 
 const styles = StyleSheet.create({
     container: {
         display: "flex",
         flexDirection: "row",
-        alignItems: "center",
         padding: 23,
-    }
+    },
+    text: {
+        fontFamily: "Inter",
+        fontStyle: "normal",
+        fontWeight: "600",
+        fontSize: 16,
+        lineHeight: 19,
+        paddingRight: 10,
+            }
 })
-export default function ActionButtons({children, onPress = () => {}}) {
+export default function ActionButtons({text, name, color, onPress = () => {}}) {
     return (
         <TouchableOpacity onPress={onPress} style={styles.container}>
-            {children}
+            <Text style={styles.text}>{text}</Text>
+            <Icon 
+             type="material-community"
+             name={name}
+             color={color}
+            />
         </TouchableOpacity>
     )
 }
