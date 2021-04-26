8dc3b6ced77b3052d62e877dfb0c41e013050f72
student <manjaka.ran@onja.org>
2021-04-26 23:50:31 +0300

    checking spelling mistake

--- src/components/NextButton/NextButtonstories.js
+++ src/components/NextButton/NextButtonstories.js
@@ -1,18 +1,14 @@
-import { action } from '@storybook/addon-actions';
-import { storiesOf } from '@storybook/react-native';
+import {action} from '@storybook/addon-actions';
+import {storiesOf} from '@storybook/react-native';
 import React from 'react';
-import { View } from 'react-native';
+import {View} from 'react-native';
 import NextButton from './NextButton';
 
 storiesOf('ActionButton', module)
-  .addDecorator((story) => <View style={{alignItems: "center"}}>{story()}</View>)
+  .addDecorator(story => <View style={{alignItems: 'center'}}>{story()}</View>)
   .add('next button', () => (
     <NextButton onPress={action('clicked next button')}>Next</NextButton>
-  ))
-  .add('add button', () => (
-    <NextButton onPress={action('clicked add button')}>Add</NextButton>
-  ))
-  .add('disabled button', () => (
-    <NextButton disabled>Add</NextButton>
-  ))
- 
+  ));
+z.add('add button', () => (
+  <NextButton onPress={action('clicked add button')}>Add</NextButton>
+)).add('disabled button', () => <NextButton disabled>Add</NextButton>);
