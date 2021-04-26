788754179df2f7ef3fadfe9c4c8a4856f5955b93
student <manjaka.ran@onja.org>
2021-04-27 00:12:09 +0300

    added style to the container and the text

--- src/components/SectionHeading/SectionHeading.js
+++ src/components/SectionHeading/SectionHeading.js
@@ -1,10 +1,23 @@
 import * as React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {Text, SafeAreaView, StyleSheet} from 'react-native';
 
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  text: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 18,
+    lineHeight: 22,
+    color: '#111827',
+  },
+});
 export default function SectionHeading({label}) {
   return (
-    <SafeAreaView>
-      <Text>{label}</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>{label}</Text>
     </SafeAreaView>
   );
 }
