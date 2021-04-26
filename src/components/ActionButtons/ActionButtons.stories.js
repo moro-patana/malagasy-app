df5996f172975136fac0e4a8593f38b324c3f871
student <manjaka.ran@onja.org>
2021-04-26 13:46:19 +0300

    created a story for each button

--- src/components/ActionButtons/ActionButtons.stories.js
+++ src/components/ActionButtons/ActionButtons.stories.js
@@ -3,35 +3,40 @@ import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import { View, Image, Text } from 'react-native';
 import ActionButtons from './ActionButtons';
-import CheckIcon from "../Icons/check-icon.png"
-import ArrowIcon from "../Icons/rightArrow.png"
-import WrongIcon from "../Icons/wrong-icon.png"
 
 storiesOf('ActionButtons', module)
   .addDecorator((story) => <View style={{alignItems: "center"}}>{story()}</View>)
   .add('Learn', () => (
-    <ActionButtons onPress={action('clicked next button')}>
-        <Text style={{color:"#06B6D4"}}>Learn</Text>
-        <Image source={ArrowIcon} style={{width: 16}}/>
-    </ActionButtons>
+    <ActionButtons 
+    onPress={action('clicked learn button')} 
+    text={"Learn"}
+    name="arrow-right"
+    color="#06B6D4"
+    />
+  ))
+  .add('Pick', () => (
+    <ActionButtons 
+    onPress={action('clicked pick button')} 
+    text={"Pick"}
+    name="arrow-right"
+    color="#06B6D4"
+    />
+  ))
+  .add('Correct', () => (
+    <ActionButtons 
+    onPress={action('clicked correct button')} 
+    text={"Correct"}
+    name="check"
+    color="#06D440"
+    />
+  ))
+  .add('Wrong', () => (
+    <ActionButtons 
+    onPress={action('clicked wrong button')} 
+    text={"Wrong"}
+    name="close"
+    color="#D4068E"
+    />
   ))
   
-   .add('Pick', () => (
-    <ActionButtons onPress={action('clicked next button')}>
-        <Text style={{color: "#06B6D4"}}>Pick</Text>
-        <Image source={ArrowIcon} style={{width: 16}}/>
-    </ActionButtons>
-  ))
-   .add('correct', () => (
-    <ActionButtons onPress={action('clicked next button')}>
-        <Text style={{color: "#06D440"}}>Correct</Text>
-        <Image source={CheckIcon} style={{width: 16}}/>
-    </ActionButtons>
-  ))
-   .add('Wrong', () => (
-    <ActionButtons onPress={action('clicked next button')}>
-        <Text style={{color: "#D4068E"}}>Wrong</Text>
-        <Image source={WrongIcon} style={{width: 16}}/>
-    </ActionButtons>
-  ))
   
