83f28c33c403ab27fdaece69b3717cbb33eba171
student <manjaka.ran@onja.org>
2021-04-26 14:52:40 +0300

    put all the tool buttons in one story

--- src/components/ToolButton/ToolButton.stories.js
+++ src/components/ToolButton/ToolButton.stories.js
@@ -1,26 +1,26 @@
 import {action} from '@storybook/addon-actions';
 import {storiesOf} from '@storybook/react-native';
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import {View} from 'react-native';
 import ToolButton from './ToolButton';
 
 storiesOf('ToolButton', module)
   .addDecorator(story => <View style={{alignItems: 'center'}}>{story()}</View>)
-  .add('Add', () => (
-    <ToolButton onPress={action('clicked add button')} name="plus" />
-  ))
-  .add('Check', () => (
-    <ToolButton onPress={action('clicked check button')} name="check" />
-  ))
-  .add('Check-all', () => (
-    <ToolButton onPress={action('clicked check-all button')} name="check-all" />
-  ))
-  .add('Go back', () => (
-    <ToolButton
-      onPress={action('clicked go back button')}
-      name="chevron-left"
-    />
-  ))
-  .add('Theme', () => (
-    <ToolButton onPress={action('clicked theme button')} name="brightness-6" />
+  .add('Tool buttons', () => (
+    <View>
+      <ToolButton onPress={action('clicked add button')} name="plus" />
+      <ToolButton onPress={action('clicked check button')} name="check" />
+      <ToolButton
+        onPress={action('clicked check-all button')}
+        name="check-all"
+      />
+      <ToolButton
+        onPress={action('clicked go back button')}
+        name="chevron-left"
+      />
+      <ToolButton
+        onPress={action('clicked theme button')}
+        name="brightness-6"
+      />
+    </View>
   ));
