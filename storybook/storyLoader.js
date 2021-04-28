d2a3088600e3280f4a679833669aa9d0c0a90a97
student <manjaka.ran@onja.org>
2021-04-28 05:39:35 +0300

    rerun the android

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -6,7 +6,9 @@
 function loadStories() {
   require('../src/components/ActionButtons/ActionButtons.stories');
   require('../src/components/Example/Example.stories');
+  require('../src/components/LanguageSwitch/LanguageSwitch.stories');
   require('../src/components/List/List.stories');
+  require('../src/components/ListItem/ListItem.stories');
   require('../src/components/MyComponent/MyComponent.stories');
   require('../src/components/PhraseTextArea/PhraseTextArea.stories');
   require('../src/components/SectionHeading/SectionHeading.stories');
@@ -18,7 +20,9 @@ function loadStories() {
 const stories = [
   '../src/components/ActionButtons/ActionButtons.stories',
   '../src/components/Example/Example.stories',
+  '../src/components/LanguageSwitch/LanguageSwitch.stories',
   '../src/components/List/List.stories',
+  '../src/components/ListItem/ListItem.stories',
   '../src/components/MyComponent/MyComponent.stories',
   '../src/components/PhraseTextArea/PhraseTextArea.stories',
   '../src/components/SectionHeading/SectionHeading.stories',
