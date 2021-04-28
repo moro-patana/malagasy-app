64394f6be96309bef6e7afd48747c676b6b67469
student <manjaka.ran@onja.org>
2021-04-28 05:38:55 +0300

    imported ListItem component, deleted unusable element and added onPress

--- src/components/List/List.stories.js
+++ src/components/List/List.stories.js
@@ -1,21 +1,16 @@
 import * as React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {View} from 'react-native';
 import {action} from '@storybook/addon-actions';
 import {storiesOf} from '@storybook/react-native';
-import {Icon} from 'react-native-elements';
 import List from './List';
-const Item = ({title}) => (
-  <View>
-    <Text>{title}</Text>
-    <TouchableOpacity onPress={action('clicked next button')}>
-      <Text>Learn</Text>
-      <Icon type="material-community" name="arrow-right" color="#06B6D4" />
-    </TouchableOpacity>
-  </View>
-);
+import ListItem from '../ListItem/ListItem';
 
 storiesOf('List', module)
   .addDecorator(story => <View style={{padding: 23}}>{story()}</View>)
   .add('List', () => (
-    <List renderItem={({item}) => <Item title={item.title} />} />
+    <List
+      renderItem={({item}) => (
+        <ListItem text={item.title} onPress={action(`${item.key}`)} />
+      )}
+    />
   ));
