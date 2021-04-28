b87db8ac78e3e9efbfe4e8c096d632a6d3440190
student <manjaka.ran@onja.org>
2021-04-28 05:37:42 +0300

    created an array of data outside the function

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,16 +2,19 @@ import * as React from 'react';
 import {FlatList} from 'react-native';
 import {SafeAreaView} from 'react-native';
 
-export default function List({renderItem}) {
+const data = [
+  {key: 1, title: 'All'},
+  {key: 2, title: 'Food'},
+  {key: 3, title: 'Greetings'},
+  {key: 4, title: 'At the restaurant'},
+  {key: 5, title: 'An unncessessarly loooong cat... '},
+  {key: 6, title: 'Single Words'},
+  {key: 7, title: 'At the market'},
+];
+export default function List({renderItem, onPress = () => {}}) {
   return (
     <SafeAreaView>
-      <FlatList
-        data={[
-          {key: 1, title: 'All'},
-          {key: 2, title: 'Food'},
-        ]}
-        renderItem={renderItem}
-      />
+      <FlatList data={data} renderItem={renderItem} onPress={onPress} />
     </SafeAreaView>
   );
 }
