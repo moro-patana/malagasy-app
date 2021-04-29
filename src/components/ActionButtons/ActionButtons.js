91fe6b8a2ad398113f738bd550e7d17772ccdaca
student <manjaka.ran@onja.org>
2021-04-29 14:59:00 +0300

    cleaned the code

--- src/components/ActionButtons/ActionButtons.js
+++ src/components/ActionButtons/ActionButtons.js
@@ -1,32 +1,28 @@
-import * as React from "react"
-import { TouchableOpacity, StyleSheet, Text } from "react-native"
-import { Icon } from "react-native-elements"
+import * as React from 'react';
+import {TouchableOpacity, StyleSheet, Text} from 'react-native';
+import {Icon} from 'react-native-elements';
 
 const styles = StyleSheet.create({
-    container: {
-        display: "flex",
-        flexDirection: "row",
-        alignItems: "center",
-        padding: 23,
-    },
-    text: {
-        fontFamily: "Inter",
-        fontStyle: "normal",
-        fontWeight: "600",
-        fontSize: 16,
-        lineHeight: 19,
-        paddingRight: 10,
-            }
-})
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 23,
+  },
+  text: {
+    fontFamily: 'Inter',
+    fontStyle: 'normal',
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 19,
+    paddingRight: 10,
+  },
+});
 export default function ActionButtons({text, name, color, onPress = () => {}}) {
-    return (
-        <TouchableOpacity onPress={onPress} style={styles.container}>
-            <Text style={[styles.text, {color: `${color}`}]}>{text}</Text>
-            <Icon 
-             type="material-community"
-             name={name}
-             color={color}
-            />
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.container}>
+      <Text style={[styles.text, {color: `${color}`}]}>{text}</Text>
+      <Icon type="material-community" name={name} color={color} />
+    </TouchableOpacity>
+  );
 }
