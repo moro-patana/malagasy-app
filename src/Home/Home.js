2154cad52077deb8c7f00993f437420afc85011f
student <manjaka.ran@onja.org>
2021-04-29 15:05:56 +0300

    imported List and ListItem

---
+++ src/Home/Home.js
@@ -0,0 +1,24 @@
+import {action} from '@storybook/addon-actions';
+import * as React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import List from '../components/List/List';
+import ListItem from '../components/ListItem/ListItem';
+import data from '../data/categories.json';
+import ToolButton from '../components/ToolButton/ToolButton';
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <List
+        data={data}
+        renderItem={({item}) => (
+          <ListItem
+            text={item.title}
+            onPress={action(`clicked ${item.title}`)}
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+}
