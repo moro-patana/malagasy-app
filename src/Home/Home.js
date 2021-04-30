d26731f4724f18e9fde7e21b40fedc38a19f44a1
student <manjaka.ran@onja.org>
2021-04-30 15:27:12 +0300

    imported the List component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,21 +3,12 @@ import * as React from 'react';
 import {Text} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import List from '../components/List/List';
-import ListItem from '../components/ListItem/ListItem';
-import data from '../data/categories.json';
 
-export default function Home() {
+export default function Home({data}) {
   return (
     <SafeAreaView>
-      <List
-        data={data}
-        renderItem={({item}) => (
-          <ListItem
-            text={item.title}
-            onPress={action(`clicked ${item.title}`)}
-          />
-        )}
-      />
+      <Text>Home</Text>
+      <List data={data} />
     </SafeAreaView>
   );
 }
