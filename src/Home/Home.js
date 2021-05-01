d48fdb7906525c2829ab35fded6d72d9046cede1
student <manjaka.ran@onja.org>
2021-05-01 08:36:54 +0300

    imported ToolButton component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,13 +1,16 @@
 import {action} from '@storybook/addon-actions';
 import * as React from 'react';
-import {Text, SafeAreaView} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import {Icon} from 'react-native-elements';
 import List from '../components/List/List';
 import data from '../data/categories.json';
+import ToolButton from '../components/ToolButton/ToolButton';
 
 export default function Home() {
   return (
     <SafeAreaView>
       <List data={data.categories} />
+      <ToolButton />
     </SafeAreaView>
   );
 }
