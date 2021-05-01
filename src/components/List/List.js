d724f3e56cbadc6e1bb82e5a664c398afc864270
student <manjaka.ran@onja.org>
2021-05-01 08:31:02 +0300

    rendered the item

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,15 +1,21 @@
+import {action} from '@storybook/addon-actions';
 import * as React from 'react';
 import {FlatList} from 'react-native';
 import {SafeAreaView} from 'react-native';
+import ListItem from '../ListItem/ListItem';
 
-export default function List({data, text, renderItem, onPress = () => {}}) {
+export default function List({data, text}) {
   return (
     <SafeAreaView>
       <FlatList
         text={text}
         data={data}
-        renderItem={renderItem}
-        onPress={onPress}
+        renderItem={({item}) => (
+          <ListItem
+            text={item.name.en}
+            onPress={action(`clicked ${item.name.en}`)}
+          />
+        )}
       />
     </SafeAreaView>
   );
