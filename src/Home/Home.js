65afea774d82f3d655d18c2276090fc1dbec2265
student <manjaka.ran@onja.org>
2021-05-01 08:30:24 +0300

    imported the list and the data

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,14 +1,13 @@
 import {action} from '@storybook/addon-actions';
 import * as React from 'react';
-import {Text} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {Text, SafeAreaView} from 'react-native';
 import List from '../components/List/List';
+import data from '../data/categories.json';
 
-export default function Home({data}) {
+export default function Home() {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
-      <List data={data} />
+      <List data={data.categories} />
     </SafeAreaView>
   );
 }
