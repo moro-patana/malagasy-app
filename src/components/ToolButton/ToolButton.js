969a1e116e21d03381e1d2958a4e6598f1703ec7
student <manjaka.ran@onja.org>
2021-05-01 10:21:40 +0300

    added style to the container and updated the margin value of the button

--- src/components/ToolButton/ToolButton.js
+++ src/components/ToolButton/ToolButton.js
@@ -1,20 +1,44 @@
 import * as React from 'react';
-import {TouchableOpacity, StyleSheet} from 'react-native';
+import {TouchableOpacity, StyleSheet, View} from 'react-native';
 import {Icon} from 'react-native-elements';
+import LanguageSwitch from '../LanguageSwitch/LanguageSwitch';
 
 const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
   button: {
     backgroundColor: '#06B6D4',
     padding: 13,
     borderRadius: 100,
-    marginTop: 20,
+    marginRight: 10,
   },
 });
 
-export default function ToolButton({name, onPress = () => {}}) {
+export default function ToolButton({type, onPress}) {
   return (
-    <TouchableOpacity onPress={onPress} style={styles.button}>
-      <Icon type="material-community" name={name} color="#FFFFFF" />
-    </TouchableOpacity>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress} style={styles.button}>
+        <Icon type={type} name="plus" color="#FFFFFF" />
+      </TouchableOpacity>
+      <LanguageSwitch
+        text1="EN"
+        type="material-community"
+        name="swap-horizontal"
+        color="#FFFFFF"
+        onPress={onPress}
+        text2="MA"
+      />
+      <TouchableOpacity onPress={onPress} style={styles.button}>
+        <Icon type={type} name="check" color="#FFFFFF" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onPress} style={styles.button}>
+        <Icon type={type} name="check-all" color="#FFFFFF" />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={onPress} style={styles.button}>
+        <Icon type={type} name="brightness-6" color="#FFFFFF" />
+      </TouchableOpacity>
+    </View>
   );
 }
