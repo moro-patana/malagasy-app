4242bb94e5f072fd396ba2b128f16a5f70367c43
student <manjaka.ran@onja.org>
2021-05-01 11:39:01 +0300

    use the value of the state to change the color

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,14 +4,16 @@ import {FlatList} from 'react-native';
 import {SafeAreaView} from 'react-native';
 import ListItem from '../ListItem/ListItem';
 
-export default function List({data, text}) {
+export default function List({data, text, textColor, listTheme}) {
   return (
     <SafeAreaView>
       <FlatList
+        style={{backgroundColor: `${listTheme}`}}
         text={text}
         data={data}
         renderItem={({item}) => (
           <ListItem
+            textColor={textColor}
             text={item.name.en}
             onPress={action(`clicked ${item.name.en}`)}
           />
