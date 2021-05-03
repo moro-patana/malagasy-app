fcc6cb9130277acee72a2a421427f6b5e3975764
student <manjaka.ran@onja.org>
2021-05-03 09:09:25 +0300

    removed props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,22 +1,17 @@
-import {action} from '@storybook/addon-actions';
 import * as React from 'react';
 import {FlatList} from 'react-native';
 import {SafeAreaView} from 'react-native';
 import ListItem from '../ListItem/ListItem';
 
-export default function List({data, text, textColor, listTheme}) {
+export default function List({data, text, onPress}) {
   return (
     <SafeAreaView>
       <FlatList
-        style={{backgroundColor: `${listTheme}`}}
+        style={{backgroundColor: '#FFFFFF'}}
         text={text}
         data={data}
         renderItem={({item}) => (
-          <ListItem
-            textColor={textColor}
-            text={item.name.en}
-            onPress={action(`clicked ${item.name.en}`)}
-          />
+          <ListItem textColor="#111827" text={item.name.en} onPress={onPress} />
         )}
       />
     </SafeAreaView>
