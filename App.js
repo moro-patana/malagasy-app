1bd6aebbd71bbdbfe60a6c3dc728b542e3df6766
student <manjaka.ran@onja.org>
2021-05-03 09:44:50 +0300

    hide the name

--- App.js
+++ App.js
@@ -12,11 +12,16 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {View, Text} from 'react-native';
 import Home from './src/Screens/Home';
 import ToolButton from './src/components/ToolButton/ToolButton';
-function DetailsScreen() {
+function DetailsScreen({name, onPress}) {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
-      <ToolButton />
+      <ToolButton
+        type="material-community"
+        name={name}
+        color="#FFFFFF"
+        onPress={onPress}
+      />
     </View>
   );
 }
@@ -25,7 +30,10 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Details" component={DetailsScreen} />
       </Stack.Navigator>
