0166e5cd9d84bc29b2ca7aa755f612eb7bd387b2
student <manjaka.ran@onja.org>
2021-05-03 09:07:45 +0300

    removed the styles

--- App.js
+++ App.js
@@ -5,107 +5,28 @@
  * @format
  * @flow strict-local
  */
-
 import React, {useState} from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {View, Text} from 'react-native';
 import Home from './src/Screens/Home';
-import List from './src/components/List/List';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+function DetailsScreen() {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
     </View>
   );
-};
+}
+const Stack = createStackNavigator();
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-  const [theme, setTheme] = useState('#E5E5E5');
-  const [textColor, setTextColor] = useState('#1F232C');
-  const [listTheme, setListTheme] = useState('##111827');
-
-  function switchTheme() {
-    if (theme === '#E5E5E5') {
-      setTheme('#1F232C');
-      setTextColor('#FFFFFF');
-      setListTheme('#111827');
-    }
-    if (theme === '#1F232C') {
-      setTheme('#E5E5E5');
-      setTextColor('black');
-      setListTheme('#FFFFFF');
-    }
-  }
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+export default function App() {
   return (
-    <View style={[{backgroundColor: `${theme}`}, {color: `${theme}`}]}>
-      <Home
-        switchTheme={switchTheme}
-        theme={theme}
-        textColor={textColor}
-        listTheme={listTheme}
-      />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+}
